7111a250b547b20cab8bc5808e0a7e10f8ca8356
Wang Guan <momocraft@gmail.com>
2018-06-19 19:04:30 +0900

    poc: get vdom from wxml.js

--- try-vm2.js
+++ try-vm2.js
@@ -3,19 +3,36 @@ const fs = require('fs');
 
 const { VM } = require('vm2');
 
-function main() {
-  const f = fs.readFileSync(path.resolve(__dirname, 'pages-index.js'), { encoding: 'utf-8' });
-  // console.log(f);
+/**
+ * @param wxmlPath {string} path passed to wcc
+ */
+function evalWxmlJs(jsPath, wxmlPath, data) {
+  const f = fs.readFileSync(jsPath, { encoding: 'utf-8' });
   const values = [];
-  const sandbox = { window: {}, __sandbox__emit: (...v) => values.push(v), };
+  const sandbox = {
+    window: {},
+    __sandbox__file: wxmlPath,
+    __sandbox__input: data,
+    __sandbox__emit: (...v) => values.push(v),
+    console: console,
+  };
+
   const vm = new VM({
     timeout: 1000,
     sandbox: sandbox,
   });
 
-  const ret = vm.run(f + '\n ; __sandbox__emit($gwx("hey.wxml"), 2);');
-  console.log(sandbox);
-  console.log(values);
+  const ret = vm.run(f + '\n ; __sandbox__emit($gwx(__sandbox__file)(__sandbox__input));');
+
+  return values[0];
+}
+
+function main() {
+  const wxml = 'page1.wxml';
+  const js = path.resolve(__dirname, 'page1.wxml.js');
+  const res = evalWxmlJs(js, wxml, { cond1: true } );
+
+  console.log(JSON.stringify(res, null, 2));
 }
 
 main();
